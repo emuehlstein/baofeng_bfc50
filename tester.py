64ff3371d2fe8977d713edf9e2cf7797566d879a
Eric Muehlstein <muehlbucks@gmail.com>
2023-09-18 12:02:00 -0500

    cleanup

--- tester.py
+++ tester.py
@@ -1,78 +1,78 @@
+""" a test script to emulate a BF-C50 programmer"""
 import serial
 
-# Define the  data (sent by computer) and ACK byte using .fromhex()
-c_read_init = bytes.fromhex("FE 03 30 08")
-c_dl_req = bytes.fromhex(
-    "45 02 50 52 4F 47 52 41 4C"
-)  # download request? data sent by computer
-ack = bytes.fromhex("06")  # ACK byte (sent by computer)
+SERIAL_PORT = "/dev/tty.usbserial-10"
 
-# Define the expected response (sent by radio) using .fromhex()
-r_init_resp = bytes.fromhex(
-    "57 03 30 08 1F 03 FF FF FF FF FF FF"
-)  # expected response to initial program request
+### DATA SENT BY COMPUTER ###
 
-# Serial port settings
-ser = serial.Serial("/dev/tty.usbserial-10", baudrate=9600)
+# initial request sent by computer
+C_READ_INIT = bytes.fromhex("FE 03 30 08")
+# download request?
+C_DL_REQ = bytes.fromhex("45 02 50 52 4F 47 52 41 4C")
+# ACK
+ACK = bytes.fromhex("06")  # ACK byte (sent by computer)
+
+### DATA SENT BY RADIO ###
+# expected response to initial program request
+R_INIT_RESP = bytes.fromhex("57 03 30 08 1F 03 FF FF FF FF FF FF")
 
 
 def send_read_init():
-    ser.write(c_read_init)
-    print("Sent c_read_init data (computer to radio):", c_read_init.hex())
+    """send the initial program request to the radio"""
+    ser.write(C_READ_INIT)
+    print("Sent C_READ_INIT data (computer to radio):", C_READ_INIT.hex())
 
 
 def check_init_resp(received_data):
-    if received_data == r_init_resp:
+    """check the response to the initial program request"""
+    if received_data == R_INIT_RESP:
         print("Received expected response (radio to computer): ", received_data.hex())
         return True
 
-    else:
-        print("Received bad init data (radio to computer): %s", received_data.hex())
-        return False
+    print("Received bad init data (radio to computer): %s", received_data.hex())
+    return False
 
 
-try:
-    send_read_init()
-    received_data = b""  # Initialize an empty byte string for concatenation
-    init_resp = ser.read(12)  # read 12 bytes (len of r_init_resp)
-    if check_init_resp(init_resp):
-        # send download request?
-        ser.write(c_dl_req)
-        print("Sent c_dl_req data (computer to radio):", c_dl_req.hex())
+if __name__ == "__main__":
 
-    while True:
-        # Read data from the serial port in chunks of 4 bytes
-        chunk = ser.read(1)
-        # print("Received data (radio to computer):", chunk.hex())
+    # Setup serial port
+    ser = serial.Serial(SERIAL_PORT, baudrate=9600)
 
-        # Concatenate the received chunk
-        received_data += chunk
+    try:
+        send_read_init()
+        received_data = b""  # Initialize an empty byte string for concatenation
+        init_resp = ser.read(12)  # read 12 bytes (len of R_INIT_RESP)
+        if check_init_resp(init_resp):
+            # send download request?
+            ser.write(C_DL_REQ)
+            print("Sent C_DL_REQ data (computer to radio):", C_DL_REQ.hex())
 
-        # Check if the first 3 received chunks match r_init_resp
-        if len(received_data) == len(r_init_resp):
-            if received_data == r_init_resp and not INIT_RESP_RVCD:
-                print(
-                    "Received expected response (radio to computer): ",
-                    received_data.hex(),
-                )
+        while True:
 
-                # flag the initial response as received
-                INIT_RESP_RVCD = True
+            print("Waiting for data from radio...")
+            # Read data from the serial port in chunks of 4 bytes
+            chunk = ser.read(1)
+            print("Received data (radio to computer):", chunk.hex())
 
-                # Send an ACK (computer to radio)
+            # Concatenate the received chunk
+            received_data += chunk
 
-                ser.write(ack)
-                print("Sent ACK (computer to radio):", ack.hex())
+            # Check if the first 3 received chunks match R_INIT_RESP
+            if len(received_data) == len(R_INIT_RESP):
+                if received_data == R_INIT_RESP and not INIT_RESP_RVCD:
+                    print(
+                        "Received expected response (radio to computer): ",
+                        received_data.hex(),
+                    )
 
-            else:
-                print("Received data (radio to computer): %s", received_data.hex())
+                else:
+                    print("Received data (radio to computer): %s", received_data.hex())
 
-            received_data = b""
+                received_data = b""
 
-        # Continue listening for more data
-except KeyboardInterrupt:
-    print("Exiting tester program.")
-except Exception as e:
-    print(f"An error occurred: {str(e)}")
-finally:
-    ser.close()
+    except KeyboardInterrupt:
+        print("Exiting tester program.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+    finally:
+        ser.close()
